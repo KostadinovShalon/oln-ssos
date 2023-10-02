f94a6cab90bbedc37469ec2e17e0565b8d177772
Brian <kostadinov.shalon@gmail.com>
2023-10-02 12:19:01 +0100

    OLN Box Cityscapes

--- configs/oln_box/oln_box_cityscapes.py
+++ configs/oln_box/oln_box_cityscapes.py
@@ -3,37 +3,9 @@ _base_ = './oln_box.py'
 # Dataset
 dataset_type = 'CocoSplitDataset'
 data_root = 'data/cityscapes/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipAug',
-        img_scale=(2048, 1024),
-        flip=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='Collect', keys=['img']),
-        ])
-]
 
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=16,
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',
@@ -43,24 +15,21 @@ data = dict(
             is_class_agnostic=True,
             ann_file=data_root +
                      'annotations/instancesonly_filtered_gtFine_train.json',
-            img_prefix=data_root,
-            pipeline=train_pipeline)),
+            img_prefix=data_root)),
     val=dict(
         type=dataset_type,
         is_class_agnostic=True,
         ann_file=data_root +
                  'annotations/instancesonly_filtered_gtFine_val.json',
-        img_prefix=data_root,
-        pipeline=test_pipeline),
+        img_prefix=data_root),
     test=dict(
         type=dataset_type,
         is_class_agnostic=True,
         ann_file=data_root +
                  'annotations/instancesonly_filtered_gtFine_val.json',
-        img_prefix=data_root,
-        pipeline=test_pipeline))
+        img_prefix=data_root))
 
-optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 
 lr_config = dict(
