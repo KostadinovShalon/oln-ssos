dc3ff65d416dfd3c7d84185341d38670a8dcddc7
Brian <kostadinov.shalon@gmail.com>
2023-10-23 13:31:01 +0100

    Fixing road anomaly 21 datasets

--- configs/oln_vos/oln_vos_mask_cityscapes_kmeans_50_test_roadanomaly21.py
+++ configs/oln_vos/oln_vos_mask_cityscapes_kmeans_50_test_roadanomaly21.py
@@ -1,2 +1,11 @@
-_base_ = ['./oln_vos_mask_cityscapes_kmeans_50.py',
-          './oln_vos_mask_cityscapes_kmeans_350_test_roadanomaly21']
+_base_ = ['./oln_vos_mask_cityscapes_kmeans_50.py']
+
+data_root = 'data/roadanomaly21/'
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=2,
+    test=dict(
+        ann_file=data_root +
+                 'instances.json',
+        img_prefix=data_root + 'images/')
+)
