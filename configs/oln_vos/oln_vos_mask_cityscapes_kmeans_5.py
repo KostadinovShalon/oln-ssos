177720d5798abb104d0ba99947832b608e1a0ab4
Brian <kostadinov.shalon@gmail.com>
2023-10-24 12:24:40 +0100

    Change oln mas learning rate

--- configs/oln_vos/oln_vos_mask_cityscapes_kmeans_5.py
+++ configs/oln_vos/oln_vos_mask_cityscapes_kmeans_5.py
@@ -47,7 +47,7 @@ data = dict(
 )
 
 custom_hooks = [dict(type='SetEpochInfoHook')]
-optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 # Pretrained on COCO
 load_from = './work_dirs/oln_mask/epoch_8.pth'
 work_dir = './work_dirs/oln_vos_mask_cityscapes_kmeans_5/'
