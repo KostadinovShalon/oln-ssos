b29a65c2629d716bba6141057a7189b5ff7a4df2
Brian <kostadinov.shalon@gmail.com>
2023-10-27 15:26:48 +0100

    oln vos mask cityscapes configs

---
+++ configs/oln_vos/oln_vos_mask_cityscapes_kmeans_5_oodlw0.5.py
@@ -0,0 +1,10 @@
+_base_ = './oln_vos_mask_cityscapes_kmeans_5.py'
+
+model = dict(
+    roi_head=dict(
+        ood_loss_weight=0.5)
+    )
+
+# Pretrained on COCO
+load_from = './work_dirs/oln_mask/epoch_8.pth'
+work_dir = './work_dirs/oln_vos_mask_cityscapes_kmeans_5_start2/'
