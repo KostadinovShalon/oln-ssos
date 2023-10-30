08a98997aec5c7e20ed309f7c996435b3251ef22
Brian <kostadinov.shalon@gmail.com>
2023-10-30 09:27:09 +0000

    Fix config of cityscapes use proposals

--- configs/oln_vos/cityscapes/oln_vos_mask_cityscapes_kmeans_5_use_proposals.py
+++ configs/oln_vos/cityscapes/oln_vos_mask_cityscapes_kmeans_5_use_proposals.py
@@ -7,4 +7,4 @@ model = dict(
 
 # Pretrained on COCO
 load_from = './work_dirs/oln_mask/epoch_8.pth'
-work_dir = './work_dirs/oln_vos_mask_cityscapes_kmeans_5_start3/'
+work_dir = './work_dirs/oln_vos_mask_cityscapes_kmeans_5_use_proposals/'
