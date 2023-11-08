c3373d70e22a56ef68c83af4b1f8c07033a5e79c
Brian <kostadinov.shalon@gmail.com>
2023-11-08 15:59:46 +0000

    VOS schedule on db6

---
+++ configs/oln_vos/db6/oln_vos_mask_db6_kmeans_5_e18_start12.py
@@ -0,0 +1,19 @@
+_base_ = "./oln_vos_mask_db6_kmeans_5.py"
+
+model = dict(
+    roi_head=dict(
+        start_epoch=5,
+    )
+)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=100,
+    warmup_ratio=0.001,
+    step=[12, 16])
+total_epochs = 18
+#
+load_from = None
+# Runner type
+runner = dict(type='PseudoLabelEpochBasedRunner')
