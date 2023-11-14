c79d844e004a8bbd7a409722cadcbec892f5e248
Brian <kostadinov.shalon@gmail.com>
2023-11-14 18:55:27 +0000

    Adding a new VOC

--- configs/oln_vos/voc_ood_coco/oln_vos_box_voc0712_kmeans_20_try_2.py
+++ configs/oln_vos/voc_ood_coco/oln_vos_box_voc0712_kmeans_20_try_2.py
@@ -3,7 +3,7 @@ model = dict(
     roi_head=dict(
         k=20,
         repeat_ood_sampling=1,
-        start_epoch=2,))
+        start_epoch=7,))
 work_dir = './work_dirs/oln_vos_box_voc0712_kmeans_20_try_2/'
 
 lr_config = dict(
@@ -11,9 +11,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[5])
-total_epochs = 7
+    step=[7])
+total_epochs = 12
 
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
-
-load_from = './work_dirs/oln_box/epoch_8.pth'
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
