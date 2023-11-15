32ed06839982b6010f4f56e0fcaa0243f0b70303
Brian <kostadinov.shalon@gmail.com>
2023-11-15 16:42:31 +0000

    Pretrained OLN-VOS for VOC-COCO

---
+++ configs/oln_vos/voc_ood_coco/oln_vos_box_voc0712_kmeans_20_start2_pretrained.py
@@ -0,0 +1,18 @@
+_base_ = './oln_vos_box_voc0712_kmeans_5_start12.py'
+model = dict(
+    calculate_pseudo_labels_from_epoch=0,
+    roi_head=dict(
+        k=20,
+        repeat_ood_sampling=1))
+work_dir='./work_dirs/oln_vos_box_voc0712_kmeans_20_start2_pretrained/'
+load_from='./work_dirs/oln_box_voc/epoch_8.pth'
+
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=100,
+    warmup_ratio=0.001,
+    step=[7])
+total_epochs = 10
+
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
