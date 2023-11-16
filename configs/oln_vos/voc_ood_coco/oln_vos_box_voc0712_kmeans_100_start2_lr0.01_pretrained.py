d7a3d9190ee785267b7c99bf3d4b715923442099
Brian <kostadinov.shalon@gmail.com>
2023-11-16 14:41:55 +0000

    Further experiments on VOC-COCO OoD

--- configs/oln_vos/voc_ood_coco/oln_vos_box_voc0712_kmeans_100_start2_lr0.01_pretrained.py
+++ configs/oln_vos/voc_ood_coco/oln_vos_box_voc0712_kmeans_100_start2_lr0.01_pretrained.py
@@ -2,10 +2,10 @@ _base_ = './oln_vos_box_voc0712_kmeans_5_start12.py'
 model = dict(
     calculate_pseudo_labels_from_epoch=0,
     roi_head=dict(
-        k=20,
+        k=100,
         start_epoch=2,
         repeat_ood_sampling=1))
-work_dir = './work_dirs/oln_vos_box_voc0712_kmeans_20_start2_pretrained/'
+work_dir = './work_dirs/oln_vos_box_voc0712_kmeans_100_start2_pretrained/'
 load_from = './work_dirs/oln_box_voc/epoch_8.pth'
 
 lr_config = dict(
@@ -16,4 +16,4 @@ lr_config = dict(
     step=[7])
 total_epochs = 10
 
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
