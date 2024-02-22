76a2beadbd58969a7265b7ca9816caf5be305714
Brian <kostadinov.shalon@gmail.com>
2024-02-22 12:31:35 +0000

    OLN VOS VOC coco configs

--- configs/oln_ffs/voc_coco/oln_vos_box_voc0712_kmeans_100_s6.py
+++ configs/oln_ffs/voc_coco/oln_vos_box_voc0712_kmeans_100_s6.py
@@ -1,3 +1,3 @@
-_base_ = './oln_ffs_box_voc0712_kmeans_100.py'
+_base_ = './oln_vos_box_voc0712_kmeans_100.py'
 
 model = dict(roi_head=dict(start_epoch=6))
---
+++ configs/oln_ffs/voc_coco/oln_vos_box_voc0712_kmeans_200_s6.py
@@ -0,0 +1,3 @@
+_base_ = './oln_vos_box_voc0712_kmeans_200.py'
+
+model = dict(roi_head=dict(start_epoch=6))
