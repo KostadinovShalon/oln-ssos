860de0453d17b453458c802fef86661184fccc28
Brian <kostadinov.shalon@gmail.com>
2024-02-28 18:24:30 +0000

    Removing masks from sixray10 base config

--- configs/_base_/datasets/sixray10_split_instance_ann_id.py
+++ configs/_base_/datasets/sixray10_split_instance_ann_id.py
@@ -13,13 +13,13 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotationsWithAnnID', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotationsWithAnnID', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='PseudoLabelFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_ann_ids', 'gt_pseudo_labels',
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_ann_ids', 'gt_pseudo_labels',
                                'gt_weak_bboxes', 'gt_weak_bboxes_labels']),
 ]
 test_pipeline = [
